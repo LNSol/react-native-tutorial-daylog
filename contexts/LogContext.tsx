7994ccf07d6b40c2dfcc32330a6b7746799da0b2
EunhaLim <dmsgk1559@gmail.com>
2023-06-06 16:01:28 +0900

    uuid 사용 및 logContext 수정

--- contexts/LogContext.tsx
+++ contexts/LogContext.tsx
@@ -1,28 +1,45 @@
 import React, {
+  PropsWithChildren,
   createContext,
   useState,
-  PropsWithChildren,
-  Dispatch,
-  SetStateAction,
   useContext,
 } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
-interface ILogContext {
-  text: string;
-  setText: Dispatch<SetStateAction<string>>;
+interface ILog {
+  id?: string;
+  title: string;
+  body: string;
+  date: Date;
 }
+interface ILogContext {
+  logs: ILog[];
+  addLog: (log: ILog) => void;
+}
+const DefaultLogContext = {
+  logs: [],
+  addLog: () => {},
+};
 
-const LogContext = createContext<ILogContext>({text: '', setText: () => {}});
+const LogContext = createContext<ILogContext>(DefaultLogContext);
 
-function LogProvider({children}: PropsWithChildren) {
-  const [text, setText] = useState('안녕하세요');
+const LogProvider = ({children}: PropsWithChildren) => {
+  const [logs, setLogs] = useState<ILog[]>([]);
+
+  const addLog = ({title, body, date}: ILog) => {
+    const newLog = {
+      id: uuidv4(),
+      title,
+      body,
+      date,
+    };
+    setLogs(prevLogs => [...prevLogs, newLog]);
+  };
 
   return (
-    <LogContext.Provider value={{text, setText}}>
-      {children}
-    </LogContext.Provider>
+    <LogContext.Provider value={{logs, addLog}}>{children}</LogContext.Provider>
   );
-}
+};
 const useLog = () => useContext(LogContext);
 
 export {LogProvider, useLog};
