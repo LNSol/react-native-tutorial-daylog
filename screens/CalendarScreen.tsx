8096c7682c3dc65691701f3ca704bf530e08fb2c
EunhaLim <dmsgk1559@gmail.com>
2023-06-09 13:55:29 +0900

    CalendarScreen에서 Animated 연습

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, View, Animated, Button} from 'react-native';
+
+const FadeInAndOut = () => {
+  const animation = useRef(new Animated.Value(1)).current;
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            opacity: animation,
+          },
+        ]}
+      />
+      <Button
+        title="FadeIn"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+      <Button
+        title="FadeOut"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 0,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+    </View>
+  );
+};
 
 const CalendarScreen = () => {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <FadeInAndOut />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   block: {},
+  rectangle: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#000',
+  },
 });
 
 export default CalendarScreen;
